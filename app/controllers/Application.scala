99575a095515a82b94de7b8d4b92edd987eca8f9
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-07-13 14:23:46 +0300

    calendar is moved to URL calendar folder

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,7 +4,7 @@ import play.api.mvc._
 
 object Application extends Controller {
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Redirect(routes.Application.calendar())
   }
 
   def calendar = Action {
