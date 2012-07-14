ac71f413e686484b6f6ec2336443a13c86091ec1
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-07-14 16:36:48 +0300

    tests for month block with parameters and for month name

--- test/controllers/MonthBlockTest.scala
+++ test/controllers/MonthBlockTest.scala
@@ -12,4 +12,25 @@ class MonthBlockTest extends Specification {
       MonthBlock().month must be equalTo Calendar.getInstance().get(Calendar.MONTH) + 1
     }
   }
+
+  "Month Block with parameters" should {
+    "contain the same year" in {
+      foreach(Seq(2000, 2012, 2100)) {
+        case (year) => MonthBlock(year, 1).year must be equalTo year
+      }
+    }
+    "contain the same month" in {
+      foreach(Seq(1, 7, 12)) {
+        case (month) => MonthBlock(1, month).month must be equalTo month
+      }
+    }
+  }
+
+  "Month Block" should {
+    "contain month name" in {
+      foreach(Seq(1 -> "January", 7 -> "July", 12 -> "December")) {
+        case (number, name) => MonthBlock(2012, number).name must be equalTo name
+      }
+    }
+  }
 }
